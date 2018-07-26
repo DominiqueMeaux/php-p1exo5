f3871624324abd8cffb1479104d81b034bc9a725
dominiquem <dominique.meaux@gmail.com>
2018-07-26 09:25:04 +0200

    php p1exo5

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>p1exo5</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $name = NULL;
+        echo $name;
+      ?>
+    </p>
+  </body>
+</html>
